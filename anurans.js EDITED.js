518d092739fb526af447c11330b117e60e89565d
Emily.Pyegon <40240733+EJchemoiwa@users.noreply.github.com>
2025-08-10 18:27:04 +1000

    Add files via upload

---
+++ anurans.js EDITED.js
@@ -0,0 +1,151 @@
+export const anuranSpecies = [
+  {
+    family: "Bufonidae",
+    genus: "Sclerophrys",
+    species: "kisoloensis",
+    commonName: "Kisolo Toad",
+    iucnLink: "https://www.iucnredlist.org/species/54977/15679510"
+  },
+  {
+    family: "Bufonidae",
+    genus: "Sclerophrys",
+    species: "gutturalis",
+    commonName: "Guttural Toad",
+    iucnLink: "https://www.iucnredlist.org/species/54699/115234756"
+  },
+  {
+    family: "Pyxicephalidae",
+    genus: "Amietia",
+    species: "nutti",
+    commonName: "Nutt’s River Frog",
+    iucnLink: "https://www.iucnredlist.org/species/113259478/175796538"
+  },
+  {
+    family: "Ptychadenidae",
+    genus: "Ptychadena",
+    species: "porosissima",
+    commonName: "Striped Rocket Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58586/177231336"
+  },
+  },
+  {
+    family: "Ptychadenidae",
+    genus: "Ptychadena",
+    species: "Ptychadena nilotica",
+    commonName: "Nile Rocket Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58675/18392547"
+  },
+  {
+    family: "Ptychadenidae",
+    genus: "Ptychadena",
+    species: "Ptychadena oxyrhynchus",
+    commonName: "Sharp-nosed Rocket Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58585/177230973"
+  },
+  {
+    family: "Ptychadenidae",
+    genus: "Ptychadena",
+    species: "anchietae",
+    commonName: "Anchieta’s Ridged Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58613/12799988"
+  },
+  {
+    family: "Pipidae",
+    genus: "Xenopus",
+    species: "Xenopus borealis",
+    commonName: "Marsabit Clawed Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58679/18393057"
+  },
+  {
+    family: "Hyperoliidae",
+    genus: "Hyperolius",
+    species: "Hyperolius viridiflavus",
+    commonName: "Common Reed Frog",
+    iucnLink: "https://www.iucnredlist.org/species/56146/112233627"
+  },
+  {
+    family: "Ranidae",
+    genus: "Amnirana",
+    species: "Amnirana albolabris",
+    commonName: "Forest White-lipped Frog",
+    iucnLink: "https://www.iucnredlist.org/species/54930/172979872"
+  },
+  {
+    family: "Dicroglossidae",
+    genus: "Hoplobatrachus",
+    species: "Hoplobatrachus occipitalis",
+    commonName: "Eastern Bullfrog",
+    iucnLink: "https://www.iucnredlist.org/species/58537/18389457"
+  },
+  {
+    family: "Phrynobatrachidae",
+    genus: "Phrynobatrachus",
+    species: "Phrynobatrachus graueri",
+    commonName: "Grauer’s Puddle Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58011/18365355"
+  },
+  {
+    family: "Phrynobatrachidae",
+    genus: "Phrynobatrachus",
+    species: "Phrynobatrachus scheffleri",
+    commonName: "Scheffler’s Puddle Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58010/18365298"
+  },
+  {
+    family: "Phrynobatrachidae",
+    genus: "Phrynobatrachus",
+    species: "Phrynobatrachus natalensis",
+    commonName: "Natal Puddle Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58014/18365435"
+  },
+  {
+    family: "Phrynobatrachidae",
+    genus: "Phrynobatrachus",
+    species: "Phrynobatrachus keniensis",
+    commonName: "Kenya Puddle Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58013/18365406"
+  },
+  {
+    family: "Leptopelidae",
+    genus: "Leptopelis",
+    species: "Leptopelis mackayi",
+    commonName: "(Mackay’s Tree Frog",
+    iucnLink: "https://www.iucnredlist.org/species/56116/112217612"
+  },
+  {
+    family: "Bufonidae",
+    genus: "Sclerophrys",
+    species: "Sclerophrys kerinyagae",
+    commonName: "(Kerinyaga Toad",
+    iucnLink: "https://www.iucnredlist.org/species/58423/18399567"
+  },
+  {
+    family: "Bufonidae",
+    genus: "Sclerophrys",
+    species: "Sclerophrys turkanae",
+    commonName: "(Turkana Toad",
+    iucnLink: "https://www.iucnredlist.org/species/54672/115231123"
+  },
+  {
+    family: "Hyperoliidae",
+    genus: "Hyperolius",
+    species: "Hyperolius cinnamomeoventris",
+    commonName: "(Cinnamon-Bellied Reed Frog",
+    iucnLink: "https://www.iucnredlist.org/species/56145/18372230"
+  },
+  {
+    family: "Ptychadenidae",
+    genus: "Ptychadena",
+    species: "Ptychadena mahnerti",
+    commonName: "(Mahnert’s Rocket Frog",
+    iucnLink: "https://www.iucnredlist.org/species/61896/12559393"
+  },
+  {
+    family: "Ptychadenidae",
+    genus: "Ptychadena",
+    species: "Ptychadena mascareniensis",
+    commonName: "(Mascarene Ridged Frog",
+    iucnLink: "https://www.iucnredlist.org/species/58677/18392890"
+  },
+  // please add the remaining ones
+];
